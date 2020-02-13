fd7ce136bfd0d404d66fb58dcf4e10653e4e3a8e
Alex Ford <flexyford@users.noreply.github.com>
2020-02-13 11:58:38 -0700

    [chore] export insertRangeBefore for DynamicRadar extensions

--- addon/-private/index.js
+++ addon/-private/index.js
@@ -7,6 +7,7 @@ export { default as StaticRadar } from './data-view/radar/static-radar';
 export { default as VirtualComponent } from './data-view/elements/virtual-component';
 
 export { default as ViewportContainer } from './data-view/viewport-container';
+export { default as insertRangeBefore } from './data-view/utils/insert-range-before';
 export { default as objectAt } from './data-view/utils/object-at';
 
 export {
